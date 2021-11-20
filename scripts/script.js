0631bd4a0de49a4172655fc8bd8b01d2f6d615da
dyossid <yossidagan@yandex.com>
2021-11-20 22:50:10 +0200

    fixes

--- scripts/script.js
+++ scripts/script.js
@@ -10,15 +10,24 @@ const editProfileSubmit = editProfileForm.querySelector("button[type=submit]");
 const profileName = document.querySelector("#profile-name");
 const profileDescription = document.querySelector("#profile-description");
 
-editProfileCloseButton.addEventListener("click", () => {
-  editProfileModal.classList.remove("modal_open");
-});
+function closeEditForm() {
+  editProfileCloseButton.addEventListener("click", () => {
+    editProfileModal.classList.remove("modal_open");
+  });
+  editProfileModal.addEventListener("click", (e) => {
+    if (e.target == editProfileModal) {
+      editProfileModal.classList.remove("modal_open");
+    }
+  });
+}
 
-openFormButton.addEventListener("click", () => {
-  editProfileModal.classList.add("modal_open");
-  nameInput.value = profileName.textContent;
-  descriptionInput.value = profileDescription.textContent;
-});
+function openEditForm() {
+  openFormButton.addEventListener("click", () => {
+    editProfileModal.classList.add("modal_open");
+    nameInput.value = profileName.textContent;
+    descriptionInput.value = profileDescription.textContent;
+  });
+}
 
 editProfileModal.addEventListener("click", (e) => {
   if (e.target === editProfileModal) {
@@ -26,10 +35,16 @@ editProfileModal.addEventListener("click", (e) => {
   }
 });
 
-editProfileForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileDescription.textContent = descriptionInput.value;
-  editProfileModal.classList.remove("modal_open");
-  editProfileForm.reset();
-});
+function changeInputForm() {
+  editProfileForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileDescription.textContent = descriptionInput.value;
+    editProfileModal.classList.remove("modal_open");
+    editProfileForm.reset();
+  });
+}
+
+openEditForm();
+closeEditForm();
+changeInputForm();
